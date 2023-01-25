b3dbc0723a6e6084cb8e0e1cd4485f051ce9163d
yitong86 <sophiajiang86@hotmail.com>
2023-01-25 18:32:39 -0500

    Initial commit

---
+++ src/main/java/models/Course.java
@@ -0,0 +1,46 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String name;
+    private String instructor;
+
+    public Course() {
+    }
+
+    public Course(Long id, String name, String instructor) {
+        this.id = id;
+        this.name = name;
+        this.instructor = instructor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
